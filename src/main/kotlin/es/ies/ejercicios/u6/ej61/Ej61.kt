50ab71434e746efdef6c7dc72ce0c732d41cdec5
Fabio <ftellol@iesrafaelalberti.es>
2026-03-04 09:00:37 +0100

    Herencia especializacion animal terminada, faltaria el main

--- src/main/kotlin/es/ies/ejercicios/u6/ej61/Ej61.kt
+++ src/main/kotlin/es/ies/ejercicios/u6/ej61/Ej61.kt
@@ -1,4 +1,5 @@
 package es.ies.ejercicios.u6.ej61
+object Ej61
 
 /**
  * Ejercicio 6.1 — Tipos de herencia, clases y subclases (RA7.a).
@@ -7,4 +8,48 @@ package es.ies.ejercicios.u6.ej61
  * Recomendación: crea subpaquetes (p. ej. `especializacion`, `extension`, etc.)
  * y añade un `main` de ejemplo que muestre polimorfismo.
  */
-object Ej61
+
+/* Los tipos de herencia elegidos son especialización y extensión */
+
+open class Animal(open val nombre: String, open val especie: String) {
+    open fun hacerSonido() {
+        println ("El animal hace un sonido")
+    }
+
+    open fun moverse() {
+        println ("El animal se mueve")
+    }
+
+    open fun comer() {
+        println ("El animal come")
+    }
+}
+
+class Perro(override val nombre: String, override val especie: String): Animal(nombre, especie) {
+    override fun hacerSonido() {
+        println ("$nombre hace guau guau")
+    }
+
+    override fun moverse() {
+        println("$nombre se mueve rapidamente")
+    }
+
+    override fun comer() {
+        println("$nombre come croquetas")
+    }
+}
+
+class Gato(override val nombre: String, override val especie: String): Animal(nombre, especie) {
+    override fun hacerSonido() {
+        println ("$nombre hace miau")
+    }
+
+    override fun moverse() {
+        println("$nombre camina muy despacio")
+    }
+
+    override fun comer() {
+        println("$nombre come pescado")
+    }
+}
+
